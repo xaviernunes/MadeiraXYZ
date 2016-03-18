24937365496cea5676b6261aefcbd0d2ab6a1810
Xavier Nunes <xaviernunes@epcc.pri>
2016-03-18 11:41:37 +0000

    bootstrap loaded to index

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!doctype html>
-<html>
+<html lang="pt_PT">
 <head>
 	<meta charset="utf-8" />
 	<title>Madeira XYZ</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Facilitamos as suas compras, vendas e pesquisas">
 	<meta name="keywords" content="compras, vendas, pesquisas">
 	<meta name="author" content="Xavier Nunes">
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->	
 </head>
 <body>
-
-	<h1>Madeira.xyz</h1>
-
+	<div class="container">
+		<header id="header" class="row">
+			<div class="col-md-12">
+				<h1>Madeira.xyz</h1>
+				<p>Facilitamos as suas compras, vendas e pesquisas</p>
+			</div>
+		</header>
+	</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
 </body>
 </html>
