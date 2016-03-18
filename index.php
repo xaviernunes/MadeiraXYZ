9c4bf81b699e7aa25d994eeb224e5095d8d2f3c8
Xavier Nunes <xaviernunes@epcc.pri>
2016-03-18 13:43:09 +0000

    added ads to index

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<meta name="author" content="Xavier Nunes">
     <link href="public/css/bootstrap.min.css" rel="stylesheet">
 	<link href="style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -16,12 +17,98 @@
 </head>
 <body>
 	<div class="container">
-		<header id="header" class="row">
-			<div class="col-md-12 text-center">
-				<h1>Madeira.xyz</h1>
-				<p>Facilitamos as suas compras, vendas e pesquisas</p>
+		<header id="header" class="row" role="header">
+			<div class="col-md-4">
+				<h1>
+					<img src="public/img/logo-madeira-XYZ.svg" alt="Madeira.xyz - Facilitamos as suas compras, vendas e pesquisas" class="logo" />
+				</h1>
 			</div>
+			<nav id="main-navigation" class="col-md-8" role="main navigation">
+				<ul>
+					<li>
+						<a href="#" title="Página inicial">Página inicial</a>
+					</li>
+					<li>
+						<a href="#" title="Diretório">Diretório</a>
+					</li>
+				</ul>
+			</nav>
 		</header>
+		<div id="content" role="page content">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Informática</h2>
+					<a href="#" title="Ver mais +" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-plus"></span> Ver mais anúncios</a>
+				</div>
+				<ul class="ads">
+					<li class="col-md-2">
+						<a class="content" href="http://madeira.xyz/anuncio/3">
+							<h2>Kingston SSDNowV300</h2>							
+							<img src="http://madeira.xyz/public/media/anuncio-3-1.png" alt="Kingston SSDNowV300" />
+							<p class="price">
+								Preço: <span>54 €<span>
+							</p>
+							<time>2016-02-17 15:19:30</time>
+						</a>
+					</li>
+					<li class="col-md-2">
+						<a class="content" href="http://madeira.xyz/anuncio/1">
+							<h2>Samsung 850 EVO - SSD</h2>							
+							<img src="http://madeira.xyz/public/media/anuncio-1.png" alt="Samsung 850 EVO - SSD" />
+							<p class="price">
+								Preço: <span>70 €<span>
+							</p>
+							<time>2016-02-17 10:05:00</time>
+						</a>
+					</li>
+					<li class="col-md-2">
+						<a class="content" href="http://madeira.xyz/anuncio/2">
+							<h2>Ram memory 8GB kit, (2 x 4GB)</h2>							
+							<img src="http://madeira.xyz/public/media/anuncio-2.png" alt="Ram memory 8GB kit, (2 x 4GB)" />			
+							<p class="price">
+								Preço: <span>82 €<span>
+							</p>			
+							<time>2016-02-17 10:05:00</time>
+						</a>
+					</li>
+					<li class="col-md-2">
+						<a class="content" href="http://madeira.xyz/anuncio/3">
+							<h2>Kingston SSDNowV300</h2>							
+							<img src="http://madeira.xyz/public/media/anuncio-3-1.png" alt="Kingston SSDNowV300" />
+							<p class="price">
+								Preço: <span>54 €<span>
+							</p>
+							<time>2016-02-17 15:19:30</time>
+						</a>
+					</li>
+					<li class="col-md-2">
+						<a class="content" href="http://madeira.xyz/anuncio/1">
+							<h2>Samsung 850 EVO - SSD</h2>							
+							<img src="http://madeira.xyz/public/media/anuncio-1.png" alt="Samsung 850 EVO - SSD" />
+							<p class="price">
+								Preço: <span>70 €<span>
+							</p>
+							<time>2016-02-17 10:05:00</time>
+						</a>
+					</li>
+					<li class="col-md-2">
+						<a class="content" href="http://madeira.xyz/anuncio/2">
+							<h2>Ram memory 8GB kit, (2 x 4GB)</h2>							
+							<img src="http://madeira.xyz/public/media/anuncio-2.png" alt="Ram memory 8GB kit, (2 x 4GB)" />			
+							<p class="price">
+								Preço: <span>82 €<span>
+							</p>			
+							<time>2016-02-17 10:05:00</time>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<footer id="footer" class="row" role="footer">
+			<div class="col-md-12">
+				&copy; <?php echo date('Y'); ?> - Madeira.xyz
+			</div>
+		</footer>
 	</div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
