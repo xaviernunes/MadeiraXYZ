670e20d1c6ca0bb8279dc0238a776c76a1103cd9
Xavier Nunes <xaviernunes@epcc.pri>
2016-03-18 11:25:07 +0000

    Index added to project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Madeira XYZ</title>
+	<meta name="description" content="Facilitamos as suas compras, vendas e pesquisas">
+	<meta name="keywords" content="compras, vendas, pesquisas">
+	<meta name="author" content="Xavier Nunes">
+</head>
+<body>
+
+	<h1>Madeira.xyz</h1>
+
+</body>
+</html>
